c371e28340b92d926a04d10131a3522642db778b
Guilherme Martins <gui.martins.soares@gmail.com>
2024-09-23 15:21:28 -0300

    Added vehicle entity files

--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using minimal_api.Interfaces;
+using minimal_api.Services;
 using MinimalApi.DTOs;
+using MinimalApi.Infra.DB;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IAdminServico, AdministradorServico>();
+
+builder.Services.AddDbContext<DbContexto>(options => {
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/login", (LoginDTO loginDTO) => {
-    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456"){
+app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdminServico administradorServico) => {
+    if (administradorServico.Login(loginDTO) != null){
         return Results.Ok("Login com sucesso");
     }
     else
